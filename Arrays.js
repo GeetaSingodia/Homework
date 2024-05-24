c9cadb30ac1e63e0b407fad7f679a19449687875
geeta <g_singodia@yahoo.com>
2024-05-24 17:54:46 -0400

    Lab30813

--- Arrays.js
+++
@@ -1,6 +0,0 @@
-let movies = ['Frozen' , 'Home Alone' , 'Barbie']
-let firstMovie = movies[0];
-console.log(movies.length);
-movies[1] = 'Batman';
-movies[movies.length]= 'Avatar';
-console.log(movies)
